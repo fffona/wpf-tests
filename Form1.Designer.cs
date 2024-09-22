a5232d681cf7249ce9484367a3f16f18eb06d504
fffona <f9n7u6@mail.ru>
2024-09-22 22:28:47 +0300

    add full working test

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button4 = new Button();
+            textBox1 = new TextBox();
+            button5 = new Button();
+            textBox2 = new TextBox();
+            button6 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(4, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(143, 62);
+            button1.TabIndex = 0;
+            button1.Text = "Назад";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(283, 262);
+            button3.Name = "button3";
+            button3.Size = new Size(230, 50);
+            button3.TabIndex = 2;
+            button3.Text = "Да";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(284, 374);
+            button2.Name = "button2";
+            button2.Size = new Size(230, 50);
+            button2.TabIndex = 3;
+            button2.Text = "Затрудняюсь ответить";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(284, 318);
+            button4.Name = "button4";
+            button4.Size = new Size(230, 50);
+            button4.TabIndex = 4;
+            button4.Text = "Нет";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(189, 180);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(424, 27);
+            textBox1.TabIndex = 5;
+            textBox1.Text = "Вопрос 1";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(283, 168);
+            button5.Name = "button5";
+            button5.Size = new Size(241, 87);
+            button5.TabIndex = 6;
+            button5.Text = "Завершить тест";
+            button5.UseVisualStyleBackColor = true;
+            button5.Visible = false;
+            button5.Click += button5_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(309, 150);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(182, 72);
+            textBox2.TabIndex = 7;
+            textBox2.Text = "Ваш результат1:\r\nбла бла бла ваш результат";
+            textBox2.Visible = false;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(257, 372);
+            button6.Name = "button6";
+            button6.Size = new Size(267, 54);
+            button6.TabIndex = 10;
+            button6.Text = "Вернуться в меню";
+            button6.UseVisualStyleBackColor = true;
+            button6.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button6);
+            Controls.Add(textBox2);
+            Controls.Add(button5);
+            Controls.Add(textBox1);
+            Controls.Add(button4);
+            Controls.Add(button2);
+            Controls.Add(button3);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button3;
+        private Button button2;
+        private Button button4;
+        private TextBox textBox1;
+        private Button button5;
+        private TextBox textBox2;
+        private Button button6;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,198 @@ namespace puirchr
 {
     public partial class Form1 : Form
     {
+        int n = 0;
+        int[] answer; // массив с выбранными вариантами ответов
         public Form1()
         {
             InitializeComponent();
+            answer = new int[11];
+        }
+        
+        // в следующей функции меняется цвет кнопки в зависимости от выбранного пользователем ответа.
+        // эта функция используется только при возврате на предыдущий вопрос
+        void colorchange1(int n)
+        {
+            switch (answer[n])
+            {
+                case 1:
+                    button3.BackColor = Color.Yellow;
+                    button4.BackColor = Control.DefaultBackColor;
+                    button2.BackColor = Control.DefaultBackColor;
+                    break;
+                case 2:
+                    button3.BackColor = Control.DefaultBackColor;
+                    button4.BackColor = Color.Yellow;
+                    button2.BackColor = Control.DefaultBackColor;
+                    break;
+                case 3:
+                    button3.BackColor = Control.DefaultBackColor;
+                    button4.BackColor = Control.DefaultBackColor;
+                    button2.BackColor = Color.Yellow;
+                    break;
+            }
+        }
+
+        void show1(int n)
+        {
+            switch (n)
+            {
+                case 0:
+                    textBox1.Text = "Вопрос 1";
+                    break;
+                case 1:
+                    textBox1.Text = "Вопрос 2";
+                    break;
+                case 2:
+                    textBox1.Text = "Вопрос 3";
+                    break;
+                case 3:
+                    textBox1.Text = "Вопрос 4";
+                    break;
+                case 4:
+                    textBox1.Text = "Вопрос 5";
+                    break;
+                case 5:
+                    textBox1.Text = "Вопрос 6";
+                    break;
+                case 6:
+                    textBox1.Text = "Вопрос 7";
+                    break;
+                case 7:
+                    textBox1.Text = "Вопрос 8";
+                    break;
+                case 8:
+                    textBox1.Text = "Вопрос 9";
+                    break;
+                case 9:
+                    textBox1.Text = "Вопрос 10";
+                    textBox1.Visible = true;
+                    button2.Visible = true;
+                    button3.Visible = true;
+                    button4.Visible = true;
+                    button5.Visible = false;
+                    break;
+                case 10:
+                    textBox1.Visible = false;
+                    button2.Visible = false;
+                    button3.Visible = false;
+                    button4.Visible = false;
+                    button5.Visible = true;
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            answer[n] = 3;
+            n++;
+            if (n > 10) n = 10;
+
+
+            // тут проверяется, отвечал ли уже пользователь на текущий вопрос.
+            // например, если пользователь дважды вернулся назад, в обоих этих вопросах должен
+            // подсвечиваться выбранный вариант ответа
+            if (answer[n] != 0)
+            {
+                colorchange1(n);
+            }
+            else
+            {
+                button3.BackColor = Control.DefaultBackColor;
+                button4.BackColor = Control.DefaultBackColor;
+                button2.BackColor = Control.DefaultBackColor;
+            }
+            show1(n);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            n--;
+            if (n < 0) n = 0;
+            show1(n);
+            colorchange1(n);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            answer[n] = 1;
+            n++;
+            if (n > 10) n = 10;
+            if (answer[n] != 0)
+            {
+                colorchange1(n);
+            }
+            else
+            {
+                button3.BackColor = Control.DefaultBackColor;
+                button4.BackColor = Control.DefaultBackColor;
+                button2.BackColor = Control.DefaultBackColor;
+            }
+            show1(n);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            answer[n] = 2;
+            n++;
+            if (n > 10) n = 10;
+            if (answer[n] != 0)
+            {
+                colorchange1(n);
+            }
+            else
+            {
+                button3.BackColor = Control.DefaultBackColor;
+                button4.BackColor = Control.DefaultBackColor;
+                button2.BackColor = Control.DefaultBackColor;
+            }
+            show1(n);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // подсчет баллов за ответы (ждем исследования Клима)
+            int correct = 0;
+            if (answer[0] == 1) correct++;
+            if (answer[1] == 1) correct++;
+            if (answer[2] == 1) correct++;
+            if (answer[3] == 1) correct++;
+            if (answer[4] == 1) correct++;
+            if (answer[5] == 1) correct++;
+            if (answer[6] == 1) correct++;
+            if (answer[7] == 1) correct++;
+            if (answer[8] == 1) correct++;
+            if (answer[9] == 1) correct++;
+
+            // вывод на экран результата
+            button5.Visible = false;
+            button1.Visible = false;
+            button6.Visible = true;
+            if (correct >= 0 && correct < 3)
+            {
+                textBox2.Text = "Ваш результат 1";
+                textBox2.Visible = true;
+            }
+            else if (correct >= 3 && correct < 6)
+            {
+                textBox2.Text = "Ваш результат 2";
+                textBox2.Visible = true;
+            }
+            else if (correct >= 6 && correct < 15)
+            {
+                textBox2.Text = "Ваш результат 3";
+                textBox2.Visible = true;
+            }
         }
     }
 }
